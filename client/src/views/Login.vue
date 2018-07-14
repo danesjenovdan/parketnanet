6fe4da7eed3f100436c101895dc1e53e2916126b
Filip Muki Dobranic <filipdobranic@gmail.com>
2018-07-14 21:15:01 +0200

    Auto stash before merge of "login" and "origin/master"

--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -22,7 +22,7 @@ export default {
         // This gives you a Google Access Token. You can use it to access the Google API.
         const token = result.credential.accessToken;
         console.log(token);
-      }).catch(function(error) {
+      }).catch((error) => {
         // Handle Errors here.
         const errorCode = error.code;
         const errorMessage = error.message;
